80c700cc7af998a7243dff5606f837fc928486c8
yamaizano <yukizakimiyamura@gmail.com>
2025-02-12 19:02:16 +0700

    script: initial script
    Signed-off-by: yamaizano <yukizakimiyamura@gmail.com>

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Indonesia /etc/localtime
+
+# Rom source repo
+repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-18.1 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b mao https://github.com/yamaizano/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=yamaizano
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_whyred-userdebug
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+m bacon
