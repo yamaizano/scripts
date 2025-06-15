91146db7ea1ee25ed40a41acbb0755e0a6813226
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-06-15 18:55:36 +0700

    whyred: lineage-20
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # Rom source repo
-repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -25,43 +25,16 @@ done
 
 # Clone local_manifests repository
 rm -r .repo/local_manifests
-git clone -b LOS19.1 https://github.com/yamaizano/local_manifests .repo/local_manifests
+git clone -b lineage-20 https://github.com/yamaizano/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
 # Sync the repositories
-rm -rf prebuilts/clang/host/linux-x86
-rm -rf prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9
-rm -r external/adeb
-rm -r external/chromium-libpac
-rm -r external/chromium-webview
-rm -r external/honggfuzz
-rm -r external/jemalloc
-rm -r external/libdaemon
-rm -r external/libunwind
-rm -r external/libunwind_llvm
-rm -r external/libvterm
-rm -r external/nfacct
-rm -r external/tinyxml
-rm -r external/u-boot
-rm -r external/v8
-rm -r frameworks/ml
-rm -r hardware/qcom/neuralnetworks/hvxservice
-rm -r packages/apps/PermissionController
-rm -r packages/apps/Terminal
-rm -r packages/modules/IPsec
-rm -r prebuilts/vndk/v27
-rm -r system/connectivity/wifilogd
-rm -r test/suite_harness
-rm -r tools/loganalysis
-rm -r tools/tradefederation/contrib
-rm -r tools/tradefederation/core
-
 /opt/crave/resync.sh
 
 rm -rf hardware/xiaomi
-git clone --depth 1 -b lineage-19.1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone --depth 1 -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 echo "============================"
 
 # Export
@@ -75,7 +48,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_whyred-user
+lunch lineage_whyred-userdebug
 echo "============="
 
 # Make cleaninstall
