e3dc5471204bdbc0d59ccd89d975c894e7b650d7
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-08-27 20:11:38 +0700

    LMAO Droid >_<
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- script.sh
+++ script.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Local TimeZone
 sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # Rom source repo
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/los-whyded/android.git -b LMODroid-fourteen --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # check if whyred tree exist
-for i in "device/xiaomi/whyred" "kernel/xiaomi/whyred" "vendor/xiaomi/whyred"
-do
+for i in "device/xiaomi/whyred" "kernel/xiaomi/whyred" "vendor/xiaomi/whyred"; do
     if [ -d "$i" ]; then
         echo "Removing directory: $i"
         rm -rf "$i"
@@ -23,7 +22,7 @@ done
 
 # Clone local_manifests repository
 rm -r .repo/local_manifests
-git clone -b lineage-21 https://github.com/yamaizano/local_manifests .repo/local_manifests
+git clone -b lmodroid https://github.com/yamaizano/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -31,11 +30,19 @@ echo "============================"
 # Sync the repositories
 /opt/crave/resync.sh
 
-rm -rf hardware/xiaomi
-git clone --depth 1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+for i in "hardware/xiaomi" "vendor/lineage-priv/keys" "vendor/lmodroid-priv/keys"; do
+    if [ -d "$i" ]; then
+        echo "Removing directory: $i"
+        rm -rf "$i"
+        git clone --depth 1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+        git clone --depth 1 -b master https://github.com/yamaizano/android_vendor_lineage-priv_keys.git vendor/lmodroid-priv/keys
+    else
+        echo "Directory not found: $i"
+        git clone --depth 1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+        git clone --depth 1 -b master https://github.com/yamaizano/android_vendor_lineage-priv_keys.git vendor/lmodroid-priv/keys
+    fi
+done
 
-rm -rf vendor/lineage-priv/keys
-git clone --depth 1 -b master https://github.com/yamaizano/android_vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 echo "============================"
 
 # Export
@@ -49,7 +56,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_whyred-ap2a-userdebug
+lunch lmodroid_whyred-ap2a-userdebug
 echo "============="
 
 # Make cleaninstall
